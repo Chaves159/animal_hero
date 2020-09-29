2f9fdd963e2cca2364782e36173d09d9569c47f4
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-29 17:33:02 -0300

    Atualizando Form de user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Usuario atualizada com sucesso' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:email,:name,:password,:password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :ongs
   resources :users, only: [:new, :create, :show, :edit]
+  post 'editar', to:'users#update'
   get 'home/index'
   get 'entrar', to:'sessions#new'
   post 'entrar', to:'sessions#create'
   delete 'sair', to:'sessions#destroy'
-  edit 'editar', to:''
+
   root to:"home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
