40c58d33330cb16691e5668499071ed92816cc4f
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-10-01 21:20:06 -0300

    Criando tabela Animais e relacionando com Ongs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,12 @@ class SessionsController < ApplicationController
   def new
     redirect_to user_path(current_user) if user_signed_in?
   end
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-
     if user && user.authenticate(params[:session][:password])
       session[:id_user] = user.id
       sign_in(user)
-      redirect_to user
+      redirect_to ongs_path
     else
       flash.now[:danger] = 'Email e Senha inválidos'
       render 'new'
@@ -20,5 +18,4 @@ class SessionsController < ApplicationController
     flash[:warning] = 'Logout realizado com sucesso'
     redirect_to entrar_path
   end
-
 end
